98cd45788227ed0f5eb6955fe4a846374fd24587
kathmarioly <katerinemarioly@gmail.com>
2017-07-07 18:16:15 -0400

    Html: modify classes and lists. Css: changing some styles. Js: performing functions (forward or backward to next page)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,36 @@
-$(document).ready(function() {
-	$('.slider-picture li').hide();
-	$('.slider-picture li:first').show();
-	
+$(document).ready(function(){
+    // Activate Carousel
+    $("#myCarousel").carousel("pause");
+
+    // Go to the previous item
+    $("#myBtn").click(function(){
+    	$("#myCarousel").carousel("prev");
+    });
+
+    // Go to the next item
+    $("#myBtn2").click(function(){
+    	$("#myCarousel").carousel("next");
+    });
+    
+    // Enable Carousel Indicators
+    $(".item1").click(function(){
+    	$("#myCarousel").carousel(0);
+    });
+    $(".item2").click(function(){
+    	$("#myCarousel").carousel(1);
+    });
+    $(".item3").click(function(){
+    	$("#myCarousel").carousel(2);
+    });
+    $(".item4").click(function(){
+    	$("#myCarousel").carousel(3);
+    });
+    
+    // control de Flechas avanzar hacia la derecha o retroceder a la izquierda
+    $(".left").click(function(){
+    	$("#myCarousel").carousel("prev");
+    });
+    $(".right").click(function(){
+    	$("#myCarousel").carousel("next");
+    });
 });
